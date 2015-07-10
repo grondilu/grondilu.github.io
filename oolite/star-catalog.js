6fc80709b75d1e7d1e9519186cc163cdf1f499a2
Lucien Grondin <grondilu@yahoo.fr>
2015-07-10 10:24:37 +0200

    aesthetic changes

--- oolite/star-catalog.js
+++ oolite/star-catalog.js
@@ -1,24 +1,24 @@
 function StarCatalog(webGLContext, catalog) {
     var shader_sources = {
-	fragment : [
-	"precision mediump float;",
-	"varying float vIntensity;",
-	"void main(void) {",
-	"    gl_FragColor = vec4(vIntensity, vIntensity, vIntensity, 1.0);",
-	"}"
-	].join("\n"),
-	vertex : [
-	"attribute vec4 aStar;",
-	"attribute float aIntensity;",
-	"uniform mat4 uMVMatrix;",
-	"uniform mat4 uPMatrix;",
-	"varying float vIntensity;",
-	"void main(void) {",
-	    "gl_PointSize = 2.0;",
-	    "gl_Position = uPMatrix * uMVMatrix * aStar;",
-	    "vIntensity = aIntensity;",
-	    "}"
-	].join("\n")
+        fragment : [
+        "precision mediump float;",
+        "varying float vIntensity;",
+        "void main(void) {",
+        "    gl_FragColor = vec4(vIntensity, vIntensity, vIntensity, 1.0);",
+        "}"
+        ].join("\n"),
+        vertex : [
+        "attribute vec4 aStar;",
+        "attribute float aIntensity;",
+        "uniform mat4 uMVMatrix;",
+        "uniform mat4 uPMatrix;",
+        "varying float vIntensity;",
+        "void main(void) {",
+            "gl_PointSize = 2.0;",
+            "gl_Position = uPMatrix * uMVMatrix * aStar;",
+            "vIntensity = aIntensity;",
+            "}"
+        ].join("\n")
     };
 
     var fragmentShader = webGLContext.createShader(webGLContext.FRAGMENT_SHADER),
@@ -39,25 +39,25 @@ function StarCatalog(webGLContext, catalog) {
     var tmpPositionArray = [ ];
     var tmpIntensityArray = [ ];
     if (Math.floor(catalog.length / 3) * 3 !== catalog.length) {
-	alert("unexpected star catalog size");
-	return;
+        alert("unexpected star catalog size");
+        return;
     } else {
-	//
-	for (var i = 0, ra, de, ma; catalog[i]; i+=3) {
-	    // expected format is Right Ascention, Declination, Magnitude
-	    ra = catalog[i];
-	    de = catalog[i+1];
-	    ma = catalog[i+2];
-	    tmpPositionArray.push(
-		    Math.cos(de)*Math.cos(ra),
-		    Math.cos(de)*Math.sin(ra),
-		    Math.sin(de),
-		    1e-6
-	    );
-	    tmpIntensityArray.push(
-		    Math.min(1.0, Math.pow(2.52, 1.1 - ma))
-	    );
-	}
+        //
+        for (var i = 0, ra, de, ma; catalog[i]; i+=3) {
+            // expected format is Right Ascention, Declination, Magnitude
+            ra = catalog[i];
+            de = catalog[i+1];
+            ma = catalog[i+2];
+            tmpPositionArray.push(
+                    Math.cos(de)*Math.cos(ra),
+                    Math.cos(de)*Math.sin(ra),
+                    Math.sin(de),
+                    1e-6
+            );
+            tmpIntensityArray.push(
+                    Math.min(1.0, Math.pow(2.52, 1.1 - ma))
+            );
+        }
     }
     var glPositionBuffer = webGLContext.createBuffer();
     webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, glPositionBuffer);
@@ -72,31 +72,31 @@ function StarCatalog(webGLContext, catalog) {
     glIntensityBuffer.numItems = tmpIntensityArray.length;
 
     this.draw = function (projectionMatrix, modelViewMatrix) {
-	webGLContext.useProgram(shaderProgram);
+        webGLContext.useProgram(shaderProgram);
 
-	// Set attributes
-	webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, glPositionBuffer);
-	webGLContext.vertexAttribPointer(
-		webGLContext.getAttribLocation(shaderProgram, "aStar"),
-		glPositionBuffer.itemSize, webGLContext.FLOAT, false, 0, 0
-		);
-	webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, glIntensityBuffer);
-	webGLContext.vertexAttribPointer(
-		webGLContext.getAttribLocation(shaderProgram, "aIntensity"),
-		glIntensityBuffer.itemSize, webGLContext.FLOAT, false, 0, 0
-		);
+        // Set attributes
+        webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, glPositionBuffer);
+        webGLContext.vertexAttribPointer(
+                webGLContext.getAttribLocation(shaderProgram, "aStar"),
+                glPositionBuffer.itemSize, webGLContext.FLOAT, false, 0, 0
+                );
+        webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, glIntensityBuffer);
+        webGLContext.vertexAttribPointer(
+                webGLContext.getAttribLocation(shaderProgram, "aIntensity"),
+                glIntensityBuffer.itemSize, webGLContext.FLOAT, false, 0, 0
+                );
 
-	// Set uniforms
-	webGLContext.uniformMatrix4fv(webGLContext.getUniformLocation(shaderProgram, "uPMatrix"), false, [
-		projectionMatrix[0], 0,  0,  0,
-		0, projectionMatrix[5],  0,  0,
-		0,                   0,  -1,  -1,
-		0,                   0,  -1, 1
-		]
-	);
-	webGLContext.uniformMatrix4fv(webGLContext.getUniformLocation(shaderProgram, "uMVMatrix"), false, modelViewMatrix);
+        // Set uniforms
+        webGLContext.uniformMatrix4fv(webGLContext.getUniformLocation(shaderProgram, "uPMatrix"), false, [
+                projectionMatrix[0], 0,  0,  0,
+                0, projectionMatrix[5],  0,  0,
+                0,                   0, -1, -1,
+                0,                   0, -1,  1
+                ]
+        );
+        webGLContext.uniformMatrix4fv(webGLContext.getUniformLocation(shaderProgram, "uMVMatrix"), false, modelViewMatrix);
 
-	// Draw points
-	webGLContext.drawArrays(webGLContext.POINTS, 0, glPositionBuffer.numItems);
+        // Draw points
+        webGLContext.drawArrays(webGLContext.POINTS, 0, glPositionBuffer.numItems);
     }
 }
