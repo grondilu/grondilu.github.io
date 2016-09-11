f012fd6347912295cb11c57cc45a01f1bd57ac78
Lucien Grondin <grondilu@yahoo.fr>
2016-09-11 21:19:26 +0200

    expand

--- cube.js
+++ cube.js
@@ -12,18 +12,18 @@ function main() {
     ];
 
     var colors = [
-	5,3,7, 5,3,7, 5,3,7, 5,3,7,
-	1,1,3, 1,1,3, 1,1,3, 1,1,3,
-	0,0,1, 0,0,1, 0,0,1, 0,0,1,
-	1,0,0, 1,0,0, 1,0,0, 1,0,0,
-	1,1,0, 1,1,0, 1,1,0, 1,1,0,
-	0,1,0, 0,1,0, 0,1,0, 0,1,0 
+        5,3,7, 5,3,7, 5,3,7, 5,3,7,
+        1,1,3, 1,1,3, 1,1,3, 1,1,3,
+        0,0,1, 0,0,1, 0,0,1, 0,0,1,
+        1,0,0, 1,0,0, 1,0,0, 1,0,0,
+        1,1,0, 1,1,0, 1,1,0, 1,1,0,
+        0,1,0, 0,1,0, 0,1,0, 0,1,0 
     ];
 
     var indices = [
-	0,1,2, 0,2,3, 4,5,6, 4,6,7,
-	8,9,10, 8,10,11, 12,13,14, 12,14,15,
-	16,17,18, 16,18,19, 20,21,22, 20,22,23 
+        0,1,2, 0,2,3, 4,5,6, 4,6,7,
+        8,9,10, 8,10,11, 12,13,14, 12,14,15,
+        16,17,18, 16,18,19, 20,21,22, 20,22,23 
     ];
 
     var vertex_buffer = gl.createBuffer();
@@ -40,21 +40,21 @@ function main() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
     var vertCode = 'attribute vec3 position;'+
-	'uniform mat4 Pmatrix;'+
-	'uniform mat4 Vmatrix;'+
-	'uniform mat4 Mmatrix;'+
-	'attribute vec3 color;'+//the color of the point
-	'varying vec3 vColor;'+
-	'void main(void) { '+//pre-built function
-	    'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);'+
-		'vColor = color;'+
-		'}';
+        'uniform mat4 Pmatrix;'+
+        'uniform mat4 Vmatrix;'+
+        'uniform mat4 Mmatrix;'+
+        'attribute vec3 color;'+//the color of the point
+        'varying vec3 vColor;'+
+        'void main(void) { '+//pre-built function
+            'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);'+
+                'vColor = color;'+
+                '}';
 
     var fragCode = 'precision mediump float;'+
-	'varying vec3 vColor;'+
-	'void main(void) {'+
-	    'gl_FragColor = vec4(vColor, 1.);'+
-		'}';
+        'varying vec3 vColor;'+
+        'void main(void) {'+
+            'gl_FragColor = vec4(vColor, 1.);'+
+                '}';
 
     var vertShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertShader, vertCode);
@@ -85,13 +85,13 @@ function main() {
     gl.useProgram(shaderprogram);
 
     function get_projection(angle, a, zMin, zMax) {
-	var ang = Math.tan((angle*.5)*Math.PI/180);//angle*.5
-	return mat4.fromValues(
-	    0.5/ang, 0 , 0, 0,
-	    0, 0.5*a/ang, 0, 0,
-	    0, 0, -(zMax+zMin)/(zMax-zMin), -1,
-	    0, 0, (-2*zMax*zMin)/(zMax-zMin), 0 
-	);
+        var ang = Math.tan((angle*.5)*Math.PI/180);//angle*.5
+        return mat4.fromValues(
+            0.5/ang, 0 , 0, 0,
+            0, 0.5*a/ang, 0, 0,
+            0, 0, -(zMax+zMin)/(zMax-zMin), -1,
+            0, 0, (-2*zMax*zMin)/(zMax-zMin), 0 
+        );
     }
 
     var proj_matrix = get_projection(40, canvas.width/canvas.height, 1, 100);
@@ -105,21 +105,21 @@ function main() {
     gl.viewport(0,0,canvas.width,canvas.height);
 
     function turn (X, Y) {
-	var
-	    X2 = X*X, Y2 = Y*Y,
-	    q = 1 + X2 + Y2,
-	    s = 1 - X2 - Y2,
-	    r2 = 1/(q*q), s2 = s*s,
-	    A = (s2 + 4*(Y2 - X2))*r2, B = -8*X*Y*r2, C = 4*s*X*r2,
-		D = (s2 + 4*(X2 - Y2))*r2, E = 4*s*Y*r2,
-		    F = (s2 - 4*(X2 + Y2))*r2;
-	mat4.multiply(mo_matrix, [
-		     A,  B, C, 0,
-		     B,  D, E, 0,
-		    -C, -E, F, 0,
-		     0,  0, 0, 1
-		    ],
-		mo_matrix);
+        var
+            X2 = X*X, Y2 = Y*Y,
+            q = 1 + X2 + Y2,
+            s = 1 - X2 - Y2,
+            r2 = 1/(q*q), s2 = s*s,
+            A = (s2 + 4*(Y2 - X2))*r2, B = -8*X*Y*r2, C = 4*s*X*r2,
+                D = (s2 + 4*(X2 - Y2))*r2, E = 4*s*Y*r2,
+                    F = (s2 - 4*(X2 + Y2))*r2;
+        mat4.multiply(mo_matrix, [
+                     A,  B, C, 0,
+                     B,  D, E, 0,
+                    -C, -E, F, 0,
+                     0,  0, 0, 1
+                    ],
+                mo_matrix);
     }
     // MOUSE MANAGEMENT
     var AMORTIZATION = 0.9;
@@ -128,45 +128,45 @@ function main() {
     var X = 0, Y = 0;
 
     canvas.addEventListener("mousedown",
-	    function (e) {
-		drag = true, old_x = e.pageX, old_y = e.pageY;
-		e.preventDefault();
-		return false;
-	    }, false
+            function (e) {
+                drag = true, old_x = e.pageX, old_y = e.pageY;
+                e.preventDefault();
+                return false;
+            }, false
     );
     canvas.addEventListener("mouseup", function (e) { drag = false });
     canvas.addEventListener("mousemove",
-	    function(e) {
-		if (!drag) return false;
-		// for the math, see http://www.texpaste.com/n/ev3443eo
-		    X = -(e.pageX-old_x)/canvas.width,
-		    Y = (e.pageY-old_y)/canvas.height;
-		turn(X, Y);
-		old_x = e.pageX, old_y = e.pageY;
-		e.preventDefault();
-	    }, false
+            function(e) {
+                if (!drag) return false;
+                // for the math, see http://www.texpaste.com/n/ev3443eo
+                    X = -(e.pageX-old_x)/canvas.width,
+                    Y = (e.pageY-old_y)/canvas.height;
+                turn(X, Y);
+                old_x = e.pageX, old_y = e.pageY;
+                e.preventDefault();
+            }, false
     );
          
 
     var animate = function () {
-	gl.clearColor(0.5, 0.5, 0.5, 0.9);
-	gl.clearDepth(1.0);
-	gl.viewport(0.0, 0.0, canvas.width, canvas.height);
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.clearColor(0.5, 0.5, 0.5, 0.9);
+        gl.clearDepth(1.0);
+        gl.viewport(0.0, 0.0, canvas.width, canvas.height);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	if (!drag) {
-	    X *= AMORTIZATION;
-	    Y *= AMORTIZATION;
-	    turn(X, Y);
-	}
-	gl.uniformMatrix4fv(_Pmatrix, false, proj_matrix);
-	gl.uniformMatrix4fv(_Vmatrix, false, view_matrix);
-	gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix);
+        if (!drag) {
+            X *= AMORTIZATION;
+            Y *= AMORTIZATION;
+            turn(X, Y);
+        }
+        gl.uniformMatrix4fv(_Pmatrix, false, proj_matrix);
+        gl.uniformMatrix4fv(_Vmatrix, false, view_matrix);
+        gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix);
 
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-	gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-					
-	window.requestAnimationFrame(animate);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+                                        
+        window.requestAnimationFrame(animate);
     }
 
     animate();
