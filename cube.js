e575aa321af9fe6e7779cd2e13cf8233c2888a46
Lucien Grondin <grondilu@yahoo.fr>
2016-09-11 20:11:28 +0200

    remove useless test

--- cube.js
+++ cube.js
@@ -110,7 +110,6 @@ function main() {
 	    q = 1 + X2 + Y2,
 	    s = 1 - X2 - Y2,
 		q2 = q**2, s2 = s**2;
-	if (!(q2 > 0)) return false;
 	var M = mat4.fromValues(
 		s2 + 4*(Y2 - X2), -8*X*Y,  4*s*X, 0,
 		-8*X*Y, s2 + 4*(X2 - Y2),  4*s*Y, 0,
