945f85ee06876d23d574a610b741d590dbf921d0
Lucien Grondin <grondilu@yahoo.fr>
2017-10-15 10:29:00 +0200

    refine oolite welcome screen

--- oolite/image-preloader.js
+++ oolite/image-preloader.js
@@ -6,24 +6,24 @@ function loadImage(url, callback) {
 }
 function loadImages(urls, callback) {
     var count = 0;
-    var debugLine = document.getElementById("debugline");
+    var debugline = document.getElementById("debugline");
     function debug(str) {
-	if (debugline) { debugline.innerHTML = str }
-	else { console.log(str) }
+        if (debugline) { debugline.innerHTML = str }
+        else { console.log(str) }
     }
     debug("loading " + urls[count++]);
     var imagesToLoad = urls.length;
     // Called each time an image finished loading.
     var onImageLoad = function() {
-	--imagesToLoad;
-	debug("loading " + urls[count++]);
-	// If all the images are loaded call the callback.
-	if (imagesToLoad == 0) {
-	    debug('all images are loaded');
-	    callback();
-	}
+        --imagesToLoad;
+        debug("loading " + urls[count++]);
+        // If all the images are loaded call the callback.
+        if (imagesToLoad == 0) {
+            debug('all images are loaded');
+            callback();
+        }
     };
     for (var ii = 0; ii < imagesToLoad; ++ii) {
-	loadImage(urls[ii], onImageLoad);
+        loadImage(urls[ii], onImageLoad);
     }
 }
