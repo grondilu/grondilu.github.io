2698ea7658d18e66403189cc7b118fd379583816
Lucien Grondin <grondilu@yahoo.fr>
2017-10-26 01:01:46 +0200

    first attempt at showing several ships

--- oolite/index.js
+++ oolite/index.js
@@ -44,6 +44,8 @@ void main()
 }`
 }
 
+let primitives = [];
+
 function start() {
     let canvas  = document.getElementById("webgl-canvas"),
         gl      = canvas.getContext("webgl"),
@@ -63,7 +65,7 @@ function start() {
             ),
             mat4.lookAt(
                 mat4.create(),
-                [0, 0, 100],
+                [0, 0, 200],
                 [0, 0, 0],
                 [0, 1, 0]
             ),
@@ -125,76 +127,9 @@ function start() {
 
     gltf
         .then(json => loadBuffers(gl, json))
+        .then(json => updatePrimitives(gl, json))
         .then(
-            function (json) {
-                let nodes   = json.nodes
-                    .filter(x => x.name.startsWith("Cobra")),
-                    primitives = [];
-                for (let node of nodes) {
-                    let mesh     = json.meshes[node.mesh];
-                    for (let primitive of mesh.primitives) {
-                        primitives.push(primitive);
-                        let attributes = primitive.attributes,
-                            vertices   = getAccessorData(json, attributes.POSITION),
-                            normals    = getAccessorData(json, attributes.NORMAL),
-                            texCoords  = getAccessorData(json, attributes.TEXCOORD_0);
-
-                        primitive.normalObject = gl.createBuffer();
-                        gl.bindBuffer(gl.ARRAY_BUFFER, primitive.normalObject);
-                        gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-
-                        primitive.texCoordObject = gl.createBuffer();
-                        gl.bindBuffer(gl.ARRAY_BUFFER, primitive.texCoordObject);
-                        gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
-
-                        primitive.vertexObject = gl.createBuffer();
-                        gl.bindBuffer(gl.ARRAY_BUFFER, primitive.vertexObject);
-                        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-                        if (defined(primitive.indices)) {
-                            let indices   = getAccessorData(json, primitive.indices);
-                            gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-                            primitive.indexObject = gl.createBuffer();
-                            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, primitive.indexObject);
-                            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-                            //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-                            //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, g.box.indexObject);
-
-                            primitive.numIndices = indices.length;
-                        }
-
-                        primitive.image   = new Image();
-                        primitive.texture = gl.createTexture();
-                        primitive.image.onload = function () {
-                            gl.bindTexture(gl.TEXTURE_2D, primitive.texture);
-                            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, primitive.image);
-                            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-                            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-                            gl.generateMipmap(gl.TEXTURE_2D)
-                            gl.bindTexture(gl.TEXTURE_2D, null);
-                        }
-                        primitive.image.crossOrigin = "anonymous";
-                        primitive.image.src =
-                            "https://raw.githubusercontent.com/OoliteProject/oolite-binary-resources/master/Textures/" +
-                            json.images[
-                                json.materials[primitive.material]
-                                .pbrMetallicRoughness
-                                .baseColorTexture
-                                .index
-                            ].uri
-                        ;
-                        primitive.emissiveFactor = primitive.image.src
-                            .includes("_diffuse") ?
-                            2.0 : 0.0
-
-                    }
-                }
-                return primitives;
-            }
-        )
-        .then(
-            function (primitives) {
+            function () {
                 let mvMatrix = matrixStack
                     .reduce((a, b) => mat4.multiply(mat4.create(), a, b)),
                     rotation = mat4.create(),
@@ -236,7 +171,7 @@ function start() {
                     for (let primitive of primitives) {
                         gl.uniform1f(
                             gl.getUniformLocation(program, "emissionFactor"),
-                            primitive.emissiveFactor
+                            primitive.emissionFactor
                         );
                         // Set up all the vertex attributes for vertices, normals and texCoords
                         gl.bindBuffer(gl.ARRAY_BUFFER, primitive.vertexObject);
@@ -253,7 +188,7 @@ function start() {
 
                         gl.drawElements(
                             gl.TRIANGLES, primitive.numIndices,
-                            gl.UNSIGNED_SHORT, 0
+                            primitive.indexType, 0
                         );
                     }
                 }
@@ -279,7 +214,7 @@ function loadBuffers(gl, gltf) {
     for (let buffer of gltf.buffers) {
         if (buffer.reader === undefined) {
             buffer.reader = new FileReader();
-            fetch("resources/models/" + buffer.uri)
+            fetch("resources/models/" + buffer.uri, {cache: "no-store"})
                 .then(response => response.blob())
                 .then(blob => buffer.reader.readAsArrayBuffer(blob));
             promises.push(
@@ -325,4 +260,82 @@ function getAccessorData(gltf, accessorIndex) {
         )(slicedBuffer);
 
     return data;
+}
+
+function updatePrimitives(gl, json) {
+    let nodes   = json.nodes
+        .filter(x => x.name.startsWith(
+            document.getElementById("shipPick").value
+        ));
+    primitives = [];
+    for (let node of nodes) {
+        let mesh     = json.meshes[node.mesh];
+        for (let primitive of mesh.primitives) {
+            primitives.push(primitive);
+            let attributes = primitive.attributes,
+                vertices   = getAccessorData(json, attributes.POSITION),
+                normals    = getAccessorData(json, attributes.NORMAL),
+                texCoords  = getAccessorData(json, attributes.TEXCOORD_0);
+
+            primitive.normalObject = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, primitive.normalObject);
+            gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+
+            primitive.texCoordObject = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, primitive.texCoordObject);
+            gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
+
+            primitive.vertexObject = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, primitive.vertexObject);
+            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+            if (defined(primitive.indices)) {
+                let indices = getAccessorData(json, primitive.indices),
+                    indexType = json.accessors[primitive.indices].componentType;
+
+                gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+                primitive.indexObject = gl.createBuffer();
+                gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, primitive.indexObject);
+                gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+                //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+                //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, g.box.indexObject);
+
+                primitive.numIndices = indices.length;
+                primitive.indexType  = 
+                    indexType == 5121 ? gl.UNSIGNED_BYTE :
+                    indexType == 5123 ? gl.UNSIGNED_SHORT :
+                    indexType == 5125 ? gl.UNSIGNED_INT :
+                    null;
+            }
+
+            primitive.image   = new Image();
+            primitive.texture = gl.createTexture();
+            primitive.image.onload = function () {
+                gl.bindTexture(gl.TEXTURE_2D, primitive.texture);
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, primitive.image);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+                gl.generateMipmap(gl.TEXTURE_2D)
+                gl.bindTexture(gl.TEXTURE_2D, null);
+            }
+            primitive.image.crossOrigin = "anonymous";
+            primitive.image.src =
+                "https://raw.githubusercontent.com/OoliteProject/oolite-binary-resources/master/Textures/" +
+                json.images[
+                    json.materials[primitive.material]
+                    .pbrMetallicRoughness
+                    .baseColorTexture
+                    .index
+                ].uri
+            ;
+            primitive.emissionFactor = primitive.image.src
+                .includes("_diffuse") ?
+                1.0 : 0.0
+
+        }
+    }
+    document.getElementById("shipPick").onchange = 
+        () => updatePrimitives(gl, json);
+    return primitives;
 }
